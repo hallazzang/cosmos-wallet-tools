5340d17df65b5bb697a273adc68a23cf99953ac6
Hanjun Kim <hallazzang@gmail.com>
2025-03-25 17:30:18 +0900

    feat: use next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="px-4 py-3 flex justify-between items-center">
+          <h1 className="text-xl">Cosmos Wallet Tools</h1>
+        </div>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [address, setAddress] = useState('');
+  const [pubKey, setPubKey] = useState('');
+
+  const handleConnectWallet = async () => {
+    if (walletConnected) {
+      setWalletConnected(false);
+      return;
+    }
+
+    await window.keplr.enable('cosmoshub-4');
+
+    const offlineSigner = await window.getOfflineSigner('cosmoshub-4');
+    const accounts = await offlineSigner.getAccounts();
+
+    setAddress(accounts[0].address);
+    setPubKey(btoa(String.fromCharCode(...accounts[0].pubkey)));
+    setWalletConnected(true);
+  };
+
+  return (
+    <div className="px-6 py-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>Public Key Viewer</CardTitle>
+          <CardDescription>
+            Connect your wallet and see the public key of it
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button onClick={handleConnectWallet}>
+            {walletConnected ? 'Disconnect' : 'Connect Wallet (Keplr)'}
+          </Button>
+          {walletConnected && (
+            <div className="mt-4">
+              <div>
+                Address:{' '}
+                <code className="font-mono font-semibold">{address}</code>
+              </div>
+              <div>
+                Public Key (base64):{' '}
+                <code className="font-mono font-semibold">{pubKey}</code>
+              </div>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
